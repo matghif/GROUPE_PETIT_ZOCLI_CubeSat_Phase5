8cec425a81a359ef7b6d2e3c1c7d30b871b56290
matzoc_pcefrei <mat.zocli@gmail.com>
2026-06-01 14:29:50 +0200

    Initialisation du projet

---
+++ src/main/java/fr/efrei/nanoorbit/NanoOrbitAppApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.nanoorbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoOrbitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoOrbitAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/efrei/nanoorbit/NanoOrbitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.nanoorbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoOrbitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
