2c192df757a3bf4a37090f5d05b3e5c399584d80
matzoc_pcefrei <mat.zocli@gmail.com>
2026-06-01 16:01:27 +0200

    Classe user

---
+++ src/main/java/fr/efrei/nanoorbit/model/User.java
@@ -0,0 +1,13 @@
+package fr.efrei.nanoorbit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private String username;
+    private String roles;
+}
