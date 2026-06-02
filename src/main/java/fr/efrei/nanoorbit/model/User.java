b56a292659ddd1d7b172351570534f98f193c12c
matzoc_pcefrei <mat.zocli@gmail.com>
2026-06-02 15:04:53 +0200

    Ajout de l'interface serializable sur le user pour gérer la sauvegarde en session

--- src/main/java/fr/efrei/nanoorbit/model/User.java
+++ src/main/java/fr/efrei/nanoorbit/model/User.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private String username;
     private String roles;
 }
